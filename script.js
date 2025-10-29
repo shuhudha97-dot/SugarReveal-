8bf764ba336a2e6f84ee73d60a6f00e94028bc31
shuhudha97-dot <shuhudha97@gmail.com>
2025-10-29 15:30:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 body \{\
+  font-family: Arial, sans-serif;\
+  padding: 20px;\
+  background-color: #f0f8ff;\
+\}\
+\
+h1 \{\
+  color: #333;\
+\}\
+\
+#search \{\
+  padding: 10px;\
+  width: 300px;\
+  margin-bottom: 20px;\
+\}\
+\
+#drink-list \{\
+  list-style: none;\
+  padding: 0;\
+\}\
+\
+#drink-list li \{\
+  background: #fff;\
+  margin-bottom: 10px;\
+  padding: 15px;\
+  border-left: 5px solid #4caf50;\
+\}\
+}
